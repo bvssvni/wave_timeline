5da37568127ccc3a9191f500fd046385d042aceb
Sven Nilsen <bvssvni@gmail.com>
2015-09-14 13:49:59 +0200

    Added slide_offset

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,11 @@ impl Timeline {
 
     pub fn draw_timeline<G: Graphics>(&self, c: &Context, g: &mut G) {
         let left_to_frame: f64 = 5.0;
-        let top_to_frame: f64 = 10.0;
+        let top_to_frame: f64 = 15.0;
         let frame_width: f64 = 10.0;
         let frame_height: f64 = 10.0;
         let frame_offset_x: f64 = 5.0;
+        let slide_offset: f64 = 0.0;
 
         // Draw bounds to see where the control is.
         {
@@ -45,8 +46,9 @@ impl Timeline {
             let rect = Rectangle::new_border([0.0, 0.0, 1.0, 1.0], 0.5);
             for i in 0..self.frames {
                 let bounds = [
-                    self.bounds[0] as f64 + left_to_frame +
-                        i as f64 * (frame_width + frame_offset_x),
+                    self.bounds[0] as f64 + left_to_frame
+                        + i as f64 * (frame_width + frame_offset_x)
+                        - slide_offset,
                     self.bounds[1] as f64 + top_to_frame,
                     frame_width,
                     frame_height
