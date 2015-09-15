2fd73e76611802cab5117e884d3eaad6d60afadf
Sven Nilsen <bvssvni@gmail.com>
2015-09-15 20:40:56 +0200

    Added `ComputedTimelineSettings`

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,43 @@ impl TimelineSettings {
     }
 }
 
+pub struct ComputedTimelineSettings {
+    pub width_for_frames: f64,
+    pub max_visible_frames: u32,
+    pub end_frame: u32,
+}
+
+impl ComputedTimelineSettings {
+    pub fn new(timeline: &Timeline, settings: &TimelineSettings)
+        -> ComputedTimelineSettings
+    {
+        use std::cmp::min;
+
+        let width_for_frames = timeline.bounds[2] as f64
+            - settings.left_to_frame - settings.right_to_frame;
+        let max_visible_frames: u32 =
+            if width_for_frames < settings.frame_width {
+                0
+            } else {
+                1
+                + (
+                    (width_for_frames - settings.frame_width)
+                    / (settings.frame_width + settings.frame_offset_x)
+                ) as u32
+            };
+        let end_frame = min(
+            timeline.frames,
+            timeline.start_frame + max_visible_frames
+        );
+
+        ComputedTimelineSettings {
+            width_for_frames: width_for_frames,
+            max_visible_frames: max_visible_frames,
+            end_frame: end_frame
+        }
+    }
+}
+
 impl Timeline {
     pub fn new_frames_bounds(frames: u32, bounds: [u32; 4]) -> Timeline {
         Timeline {
@@ -70,23 +107,14 @@ impl Timeline {
         let frame_offset_x: f64 = self.settings.frame_offset_x;
         let left_to_goto_beginning: f64 = self.settings.left_to_goto_beginning;
         let right_to_goto_end: f64 = self.settings.right_to_goto_end;
+
+        let computed_settings = ComputedTimelineSettings::new(self, &self.settings);
+
         let slide_offset: f64 = 0.0;
-        let width_for_frames = self.bounds[2] as f64
-            - left_to_frame - right_to_frame;
-        let max_visible_frames: u32 =
-            if width_for_frames < frame_width {
-                0
-            } else {
-                1
-                + (
-                    (width_for_frames - frame_width)
-                    / (frame_width + frame_offset_x)
-                ) as u32
-            };
-        let end_frame = min(
-            self.frames,
-            self.start_frame + max_visible_frames
-        );
+
+        let width_for_frames = computed_settings.width_for_frames;
+        let max_visible_frames: u32 = computed_settings.max_visible_frames;
+        let end_frame = computed_settings.end_frame;
 
         // Draw bounds to see where the control is.
         {
